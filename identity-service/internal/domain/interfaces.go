3430d3a2f4a671953239b26ed7636b66e4c1dbd4
dmmhlchk <dmmhlchk@gmail.com>
2026-05-27 17:16:09 +0600

    determine common use cases + signup was implemented

--- identity-service/internal/domain/interfaces.go
+++ identity-service/internal/domain/interfaces.go
@@ -6,15 +6,18 @@ type UserRepository interface {
 	FindByID(ctx context.Context, id int) (*User, error)
 	FindByUsername(ctx context.Context, username string) (*User, error)
 	FindByPhone(ctx context.Context, phone string) (*User, error)
-	Create(ctx context.Context, user *User)
-	Update(ctx context.Context, user *User)
-	Delete(ctx context.Context, user *User)
+
+	ExistsByPhoneOrusername(ctx context.Context, phone, username string) (bool, error)
+
+	Create(ctx context.Context, user *User) error
+	Update(ctx context.Context, user *User) error
+	Delete(ctx context.Context, user *User) error
 }
 
 type SessionRepository interface {
-	Create(ctx context.Context, session *Session)
-	Update(ctx context.Context, session *Session)
-	Delete(ctx context.Context, session *Session)
+	Create(ctx context.Context, session *Session) error
+	Update(ctx context.Context, session *Session) error
+	Delete(ctx context.Context, session *Session) error
 }
 
 type PasswordHasher interface {
---
+++ identity-service/internal/usecase/user_signup.go
@@ -0,0 +1,71 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"identity-service/internal/domain"
+)
+
+// 1. Determine Input and Output data
+// 1.1. SignUpInput defines the data required to register a new user
+type SignUpInput struct {
+	Username string
+	Phone    string
+	Password string
+}
+
+// 1.2. SignUpOutput defines waht data we return to the delivery layer upon success
+type SignUpOutput struct {
+	UserId int
+}
+
+// 2. Determine the dependencies
+// 2.1. SignUpUseCase coordinates domain layer to register a user
+type SignUpUseCase struct {
+	userRepository domain.UserRepository
+	passwordHasher domain.PasswordHasher
+}
+
+// 2.2. NewSignUpUseCase is a constructor that handles dependency injection
+func NewSignUpUseCase(repo domain.UserRepository, hasher domain.PasswordHasher) *SignUpUseCase {
+	return &SignUpUseCase{
+		userRepository: repo,
+		passwordHasher: hasher,
+	}
+}
+
+// 3. Execute runs the actual step-by-step registration business flow
+func (uc *SignUpUseCase) Execute(ctx context.Context, input SignUpInput) (*SignUpOutput, error) {
+	// 1. Validate input basic constraints
+	if input.Username == "" || input.Phone == "" || input.Password == "" {
+		return nil, errors.New("required fields were not filled")
+	}
+
+	// 2. Check the domain repository to see if this user already exists
+	exists, err := uc.userRepository.ExistsByPhoneOrusername(ctx, input.Phone, input.Username)
+	if err != nil {
+		return nil, fmt.Errorf("failed to verify account uniqueness: %w", err)
+	}
+	if exists {
+		return nil, errors.New("phone number or username is already registered")
+	}
+
+	// 3. Secure the password using our domain's abstract PasswordHasher interface
+	hashedPassword, err := uc.passwordHasher.Hash(input.Password)
+	if err != nil {
+		return nil, fmt.Errorf("failed to process password: %w", err)
+	}
+
+	// 4. Register a new user
+	newUser := domain.NewUser(0, input.Username, input.Phone, hashedPassword)
+	err = uc.userRepository.Create(ctx, newUser)
+	if err != nil {
+		return nil, fmt.Errorf("failed to save user: %w", err)
+	}
+
+	// 5. Return success
+	return &SignUpOutput{
+		UserId: newUser.ID,
+	}, nil
+}
